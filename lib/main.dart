53ff0cf9012be48c882557dddf67a004e4399cba
luiscornejo1 <luiscarlocornejor@live.com>
2024-09-17 02:49:55 -0500

    agregando primeras vistas

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:miosense/Views/splash_screen.dart';
+
 
 void main() {
-  runApp(const MainApp());
+  runApp(MiosenseApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MiosenseApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
     );
   }
 }
